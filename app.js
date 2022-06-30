8af32df32da005e93e89846d1ea4cb6a801e8a37
Meet Rajendrakumar Patel <mt750710@dal.ca>
2022-06-30 20:07:14 -0300

    New changes

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+const s3 = new AWS.S3({
+    accessKeyId: "ASIASAMRWUJ4SZEC5KFY",
+    secretAccessKey: "B6nYeTiaC9h/r678Ew+/z49fI9Fm9x3IKHu7RCS9",
+    sessionToken:
+      "FwoGZXIvYXdzECAaDJRSopz2XAUG646HMCLAAW0mxW6gvdQO3SdVhJ5cmN+sxUpsvbAR3pclH1pCA70giJOMD0ZVw0TicvO1T2BqRzPXmP9kPfgs2dTWl+RHqUb/rSDAjlvBLVHx+D5x3gZ/X+bHKbehuywO4bOaHLITJLogn3oeF5fB0Jv6TnYyv/ZGsl1hl6OkJ9dR/1CaQkIBuC1QN6F/49sa0LVsynon7LzUw/EEsWmzcF+VUo0CZ4cP41Byt4zl/yX+ii1id/nonYQYNaoWmrkga3BLfbbsFSil1/iVBjItl6Z5ii10Q7t+FCUQ9/I25G62OV/aDyCSo4Z/UE1sPAIRFpoBZUZ2Dbv32Oso",
+  });
+
 app.get('/begin', async function(req,res){
     console.log("start point");
     const response = await fetch('http://52.23.207.11:8081/start', {
@@ -31,30 +38,104 @@ app.get('/begin', async function(req,res){
 });
 
 
-app.post('/storedata',async function(req,res){
-    console.log("Start point");
-    const data = req.body.data;
-    const awsResponse = await putInAWSS3(data);
-    console.log(awsResponse.Location);
-    res.json({ "s3uri": awsResponse.Location});
-});
-
-const s3 = new AWS.S3({
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    sessionToken: AWS_SESSION
+app.post("/storedata", function (req, res) {
+    var body = req.body;
+    console.log(body);
+    const fileName = "summerdevil.txt";
+  
+    fs.writeFile(fileName, body.data.toString(), function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    });
+  
+    if (!body.data) {
+      res.send(
+        JSON.stringify({
+          file: null,
+          error: "Invalid JSON input.",
+        })
+      );
+    } else {
+      const params = {
+        Bucket: "b00897765-data",
+        Key: `${fileName}`,
+        Body: body.data,
+      };
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(`Successfully uploaded package.${data.Location}`);
+        res.setHeader("Content-Type", "application/json");
+        return res.status(200).send(
+          JSON.stringify({
+            file: fileName,
+            s3uri: `https://${bucketname}.s3.amazonaws.com/${fileName}`,
+            Success: "Data written on s3 successfully uploaded.",
+            statusCode: 200,
+          })
+        );
+      });
+    }
   });
-  const putInAWSS3 = async (data) => {
-    return s3
-    .upload({
-        Body: data,
-        Bucket: AWS_BUCKET_NAME,
-        Key: "test.txt",
-        ACL:'public-read'
-    })
-    .promise();    
-};
-
+  
+  app.post("/appenddata", function (req, res) {
+    var body = req.body.data;
+    const fileName = "summerdevil.txt";
+    const Bucketname = "b00897765-data";
+    console.log(body);
+    const getParams = {
+      Bucket: Bucketname,
+      Key: `${fileName}`
+  }
+  
+  s3.getObject(getParams, function (err, reqdata) {
+          if (err) {
+              console.log(err);
+          } else {
+              data = reqdata.Body.toString().concat(" ",body);
+              const params2 = {
+                  Bucket: Bucketname,
+                  Key: `${fileName}`,
+                  Body: data
+              }
+  
+              s3.upload(params2, function (err, data){
+                  res.setHeader("Content-type","application/JSON");
+                  res.status(200).send;
+                  res.send(JSON.stringify({
+                      s3uri : data.Location
+                  }));
+              });
+          }
+  });
+  });
+  
+  app.post("/deletefile", function (req, res) {
+    var body = req.body;
+    const fileName = "jenishsas.txt";
+    var params = {
+      Bucket: bucketname,
+      Key: fileName,
+    };
+    s3.deleteObject(params, function (err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+      } else {
+        console.log(`Successfully deleted file from s3`);
+        console.log("File not found...");
+        res.setHeader("Content-Type", "application/json");
+        return res.status(200).send(
+          JSON.stringify({
+            file: fileName,
+            s3uri: `https://${bucketname}.s3.amazonaws.com/${fileName}`,
+            Success: `${fileName} successfully deleted on s3 `,
+            statusCode: 200,
+          })
+        );
+      }
+    });
+  });
   app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
 
 /* References of code 
