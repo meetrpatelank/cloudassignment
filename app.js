938bda36bb04392c7e1852b895c1afcfa0f46ea1
Meet Rajendrakumar Patel <mt750710@dal.ca>
2022-06-30 20:18:30 -0300

    New changes

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+var fs = require("fs");
+
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
